32a60b49cba17022f2d6ca522bf382142cf1ccd4
ikiran-dev <ikiranrockrzz@gmail.com>
2023-10-24 18:08:27 +0530

    banner completed

---
+++ src/Components/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Button.css";
+import arrow from "/arrow.png";
+
+function Button({ link, text }) {
+  return (
+    <>
+      <a href={link}>
+        {text} <img src={arrow} alt="" />
+      </a>
+    </>
+  );
+}
+
+export default Button;
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css";
 import logo from "/LeadPulse_Logo.png";
 import logo_light from "/LP_LIGHT.png";
 import SwitchButton from "./SwitchButton";
+import Button from "./Components/Button";
 import "./SwitchButton.css";
 
 function Home({ state }) {
@@ -25,6 +26,15 @@ function Home({ state }) {
           <img src={isChecked ? logo_light : logo} alt="" />
           <SwitchButton idname="Menu" state={setIsmenu} />
         </nav>
+      </div>
+      <div className="hero">
+        <h1>
+          We <span>craft</span> integrated marketing technology solutions &
+          digital interfaces that <span>captivate</span> audiences and&nbsp;
+          <span>drive </span>
+          conversions.
+        </h1>
+        <Button link={"#"} text={"Learn More"} />
       </div>
     </div>
   );
